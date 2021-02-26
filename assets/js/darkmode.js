bf5dba61d2e44ea2e19b768e2cebc1b30dd976b9
Phan Huong <phanngochuong@hotmail.com>
2021-02-26 19:05:15 +0100

    Refactor darkmode.js
    Add comments

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,4 +1,6 @@
-// Dark mode
+/**
+ * Dark mode
+ */
 let dark_mode = document.getElementById("dark_mode");
 let dark_mode_icon = document.createElement("img");
 dark_mode_icon.setAttribute("src", "assets/images/night.png");
@@ -9,13 +11,17 @@ dark_mode_icon.onclick = function (){
     dark_mode.classList.toggle("night");
 
     var is_night = dark_mode.classList.contains("night");
+    // Set background color to dark grey when dark mode is on
+    // and change the icon to day-mode
     if (is_night) {
         this.setAttribute("src", "assets/images/day.png");
         this.style.backgroundColor = "#666666";
         mute_icon.style.backgroundColor = "#666666";
+    // Set background color to white when dark mode is on
+    // and change the icon to night-mode
     } else {
         this.setAttribute("src", "assets/images/night.png");
         this.style.backgroundColor = "#333333";
         mute_icon.style.backgroundColor = "#333333";
     }
-};
+}
