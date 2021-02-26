bf255ab29ef060e38f882884e20a2084fc32d7c2
Phan Huong <phanngochuong@hotmail.com>
2021-02-26 19:00:11 +0100

    Refactor soundmode.js
    Edit variable names, change horizontal format

--- assets/js/soundmode.js
+++ assets/js/soundmode.js
@@ -1,27 +1,28 @@
-// Sounds
-var bleep = new Audio();
-bleep.src = "assets/C3.mp3";
+/**
+ * Play sound
+ */
+var sound = new Audio();
+sound.src = "assets/C3.mp3";
 function play_sound() {
-    bleep.play(); // Play button sound now
+    // Play button sound at the moment a button is pressed
+    sound.play();
 }
 
-
-//mute
+/**
+ * Mute mode
+ */
 let enableMute = document.getElementById("enableMute");
-let mute_icon   =document.createElement("img");
+let mute_icon = document.createElement("img");
 mute_icon.setAttribute("src", "assets/images/mute.png");
 enableMute.appendChild(mute_icon);
 mute_icon.onclick = function (){
     enableMute.classList.toggle("mute");
-
     var is_muted = enableMute.classList.contains("mute");
-
     if(is_muted){
         bleep.muted = true;
         this.setAttribute("src", "assets/images/play.png");
-    
-    } else {
+        } else {
         this.setAttribute("src", "assets/images/mute.png");
-        bleep.muted=false
+        bleep.muted = false
     }
 }
